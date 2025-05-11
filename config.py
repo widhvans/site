b4d14a06328279931b6b0aca81d18978f629897b
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-11 14:17:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
-TELEGRAM_BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Replace with your Telegram bot token
+TELEGRAM_BOT_TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"  # Replace with your Telegram bot token
 VPS_IP = "116.203.92.20"  # Your VPS IP
 SITE_URL = f"http://{VPS_IP}"
